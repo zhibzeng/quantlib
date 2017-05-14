afb8024e91dfe54c5845f462328fac57c16796bc
SnowWalkerJ <jike3212001@163.com>
2017-05-14 17:41:10 +0800

    配置文件和本地化

---
+++ quant/common/localize.py
@@ -0,0 +1,111 @@
+"""用来本地化数据的模块"""
+import os
+import pickle
+import functools
+from inspect import signature
+import pandas as pd
+from .settings import DATA_PATH, MAIN_PATH
+
+
+class Register:
+    """
+    用来记录有哪些数据被本地化以及本地化时调用的函数、使用的参数，以便于日后的跟踪维护
+    """
+    def __init__(self, path="register.pkl"):
+        self.log_path = os.path.join(MAIN_PATH, path)
+        try:
+            with open(self.log_path, "rb") as file_handler:
+                self.data = pickle.load(file_handler)
+        except FileNotFoundError:
+            self.data = dict()
+
+    def keys(self):
+        """List the keys of the register"""
+        return self.data.keys()
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def register(self, func, path, args, kwargs, time=None, exclude=None):
+        """
+        记录本地化数据的来源（函数、参数）以及目标（文件名、键名），便于日后更新
+        """
+        exclude = exclude or []
+        sig = signature(func)
+        bounded = sig.bind(*args, **kwargs)
+        bounded.apply_defaults()
+        keys = ["x"] + [
+            value for pname, value in bounded.arguments.items() if isinstance(value, str) and pname not in exclude
+        ]
+        key = "_".join(keys)  # 将函数的所有字符串参数连接起来作为hdf的key
+        module = func.__module__
+        func_name = func.__name__
+        meta_key = "%s/%s/%s" % (module, func_name, key)
+        if meta_key not in self.data:
+            self.data[meta_key] = {
+                "module": module,
+                "name": func_name,
+                "key": key,
+                "params": bounded,
+                "path": path,
+                "time": time,
+                "exclude": exclude,
+            }
+            self.save()
+        return key
+
+    def save(self):
+        """Save the register data to pickle file"""
+        with open(self.log_path, "wb") as file_handler:
+            pickle.dump(self.data, file_handler)
+
+
+class LocalizeWrapper:
+    """
+    这是一个装饰器，使用@localizer.wrap(filename)来装饰一个函数，该函数应该返回pandas对象。
+    则每次调用函数时会优先调用pd.read_hdf(filename, key)，key是函数的字符串参数的拼接，从本地读取缓存，如果读取失败才调用原函数，并将返回值储存在对应的文件内。
+    """
+
+    def __init__(self, path: str):
+        self.path = path
+
+    def wrap(self, filename=None, time=None, exclude=None):
+        """装饰器，被装饰过的函数都会自动本地化
+        Args:
+            filename (str):数据要保存的h5文件名
+            time (str): 对应于数据时间的参数名，便于日后更新数据
+            exclude (list): 哪些参数不需要记录到键名中
+        Example:
+            @LOCALIZER.wrap("data")
+            def get_data(code):
+                ...
+            会自动把函数的返回值以`code`为键本地化到`data.h5`中
+        """
+        def true_wrapper(fn):
+            nonlocal filename
+            filename = filename or fn.__name__
+            if not filename.endswith("h5"):
+                filename = filename + ".h5"
+            path = os.path.join(self.path, filename)  # 将指定的文件名或函数名作为文件名
+
+            @functools.wraps(fn)
+            def wrapped(*args, **kwargs):
+                """装饰后的函数"""
+                key = REGISTER.register(fn, path, args, kwargs, time=time, exclude=exclude)
+                # 从注册器注册该函数及参数，并获得对应的键名
+                try:
+                    data = pd.read_hdf(path, key)
+                except (FileNotFoundError, KeyError):
+                    data = fn(*args, **kwargs)
+                    data.to_hdf(path, key)
+                return data
+
+            # return decorate(fn, wrapped)
+            return wrapped
+
+        return true_wrapper
+
+
+
+LOCALIZER = LocalizeWrapper(DATA_PATH)
+REGISTER = Register()
---
+++ quant/common/settings.py
@@ -0,0 +1,116 @@
+"""
+Global settings about where to store data and configurations.
+"""
+import os
+from argparse import ArgumentParser
+
+MAIN_PATH = os.path.abspath(os.path.expanduser("~/.quantlib"))
+CONFIG_PATH = os.path.join(MAIN_PATH, "config.cfg")
+DATA_PATH = os.path.join(MAIN_PATH, "data")
+
+
+class ConfigManager:
+    """配置项管理，解析配置文件，并允许配置项被命令行参数重写"""
+    def __init__(self, path="config.cfg"):
+        self.path = path
+        self.parser = ArgumentParser()
+        self.ready = False
+        with open(self.path, "r") as config_file:
+            self.data = self.__parse_config_file(config_file)
+
+    def __parse_config_file(self, file):
+        cfg = {}
+        for line in file:
+            stripped = line.strip()
+            if not stripped or stripped.startswith("#"):
+                continue
+            pair = stripped.split("=")
+            if len(pair) != 2:
+                raise RuntimeError("Config format error: `%s`" % line)
+            key = pair[0].strip()
+            value = pair[1].strip()
+            help_text = ""
+            if "#" in value:
+                i = value.index("#")
+                help_text = value[i+1:].strip()
+                value = value[:i].strip()
+            true_value = None
+            for parser in (int, float, self.__boolparser, self.__strparser):
+                try:
+                    true_value = parser(value)
+                except ValueError:
+                    pass
+                else:
+                    break
+            if true_value is None:
+                raise ValueError("unexpected error in config: `%s`" % line)
+            cfg[key] = {'default': true_value, 'type': type(true_value)}
+            self.parser.add_argument("--%s" % key, default=true_value, type=type(true_value), help=help_text)
+        return cfg
+
+    @staticmethod
+    def __strparser(unparsed_value):
+        if unparsed_value[0] == unparsed_value[-1] == "'" \
+            or unparsed_value[0] == unparsed_value[-1] == "\"":
+            return unparsed_value[1: -1]
+        return unparsed_value
+
+    @staticmethod
+    def __boolparser(unparsed_value):
+        if unparsed_value == "False":
+            return False
+        elif unparsed_value == "True":
+            return True
+        else:
+            raise ValueError
+
+    def __getattr__(self, item):
+        if not self.ready:
+            self.update()
+        try:
+            return self.data[item]['value']
+        except KeyError:
+            return self.data[item]['default']
+
+    def add_argument(self, *args, **kwargs):
+        """除了配置文件已有的参数外，新增命令行参数"""
+        # TODO: 如果新增的参数和配置文件中的重复，可能有冲突
+        self.parser.add_argument(*args, **kwargs)
+
+    def keys(self):
+        """列出所有可用的参数名"""
+        return self.data.keys()
+
+    def update(self):
+        """从命令行参数中更新所有配置"""
+        args = self.parser.parse_args()
+        for key, value in args._get_kwargs():
+            try:
+                self.data[key]['value'] = value
+            except KeyError:
+                self.data[key] = {'value': value, 'type': type(value)}
+        self.ready = True
+
+
+
+def create_default_config():
+    """Create default config file"""
+    with open(CONFIG_PATH, "w") as config_file:
+        config_file.writelines([
+            "wind_db_driver = 'pymysql'",
+            "wind_host = 'localhost'",
+            "wind_username = 'wind'",
+            "wind_password = 'password'",
+        ])
+
+
+def make_default_settings():
+    """Create directories for data and configurations"""
+    os.mkdir(MAIN_PATH)
+    os.mkdir(DATA_PATH)
+    create_default_config()
+
+
+if not os.path.exists(MAIN_PATH):
+    make_default_settings()
+CONFIG = ConfigManager(os.path.join(MAIN_PATH, "config.cfg"))
