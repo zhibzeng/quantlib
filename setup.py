9f825f2d245dad2bf8faeaf6ea205ac6af525d04
SnowWalkerJ <chen.bingxuan@confiance-capital.com>
2017-05-16 15:34:00 +0800

    update information

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 setup(
     name='quantlib',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test", "script"]),
     # scripts=["scripts/quantlib"],
     url='',
     license='BSD',
-    author='HenryXiang, SnowWalkerJ',
-    author_email='xiangcctv@126.com, jike3212001@163.com',
+    author='SnowWalkerJ',
+    author_email='jike3212001@163.com',
     description='',
 )
