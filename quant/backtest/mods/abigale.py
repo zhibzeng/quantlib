e6673cb2264d419710354f97933ade2492b3129c
SnowWalkerJ <jike3212001@163.com>
2017-09-26 11:21:14 +0800

    there's some problem with auto plug-in. This is a temperory solution

--- quant/backtest/mods/abigale.py
+++ quant/backtest/mods/abigale.py
@@ -25,6 +25,10 @@ class AbigaleMod(AbstractMod):
         self.metadata = metadata or {}
         super(AbigaleMod, self).__init__()
 
+    def __plug_in__(self, caller):
+        self.strategy = caller
+        caller.event_manager.register(EventType.BACKTEST_FINISH, self.on_backtest_finish)
+
     @classmethod
     def register_factor(cls, factor):
         cls.risk_factors.add(factor)
