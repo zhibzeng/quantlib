4902fc9a7525d5e80c199cd167922a1004b212f8
SnowWalkerJ <jike3212001@163.com>
2018-05-03 13:33:57 +0800

    use total capital instead of liquid capital

--- quant/analysis/barra/entities/portfolio.py
+++ quant/analysis/barra/entities/portfolio.py
@@ -9,7 +9,7 @@ class Portfolio:
 
     def get_returns(self):
         if self._rtn is None:
-            stocks_rtn = wind.get_wind_data("AShareEODPrices", "s_dq_pctchange") / 100
+            stocks_rtn = wind.get_wind_data("AShareEODPrices", "s_dq_pctchange").loc["2003-01-01":] / 100
             self._rtn = (stocks_rtn * self.weight).dropna(how="all").sum(1)
         return self._rtn
 
@@ -18,8 +18,8 @@ __estimation_universe = None
 def get_estimation_universe():
     global __estimation_universe
     if __estimation_universe is None:
-        size = wind.get_wind_data("AShareEODDerivativeIndicator", "s_dq_mv")
-        weight = pd.DataFrame({idx: row / row.sum() for idx, row in size.iterrows()}).T
+        size = wind.get_wind_data("AShareEODDerivativeIndicator", "s_val_mv")
+        weight = pd.DataFrame({idx: row / row.sum() for idx, row in size.iterrows()}).T.dropna(how='all')
         __estimation_universe = Portfolio(weight)
     return __estimation_universe
 
